622b14466642aa34671663f961ed0ae18db35244
kenosborne <ken.osborne.iii@gmail.com>
2020-01-29 14:57:34 -0800

    function to generate statistics by game

--- R/generate_play_stats.R
+++ R/generate_play_stats.R
@@ -38,3 +38,36 @@ generate_player_stats <- function(play_df){
     df
 
 }
+
+
+#' Generate Game Statistics
+#'
+#' Get statistics for all the unique games played.
+#'
+#' Input a play df. Return a data frame of statistics characterizing the play
+#' patterns of each individual game.
+#' @param play_df a data frame of valid plays
+#' @export
+generate_game_stats <- function(play_df) {
+
+    df <- play_df %>%
+        group_by(game) %>%
+        summarise(
+            chair_hours = sum(length) / 60,
+            play_count = length(unique(id)),
+            player_count = length(unique(name)),
+            locations = length(unique(location))
+        ) %>%
+        arrange(desc(chair_hours))
+
+    play_time <- play_df %>%
+        group_by(id, length, game) %>%
+        summarise(player_count = length(unique(name))) %>%
+        group_by(game) %>%
+        summarise(hours_played = sum(length) / 60)
+
+    df <- left_join(df, play_time, by = "game")
+
+    df
+
+}
