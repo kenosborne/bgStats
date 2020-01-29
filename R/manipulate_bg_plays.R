9990c3772a3aa70e374340b4f8dd1ffaf7b62a19
kenosborne <ken.osborne.iii@gmail.com>
2020-01-29 13:55:57 -0800

    make adding winner data a part of creating an data frame of plays

--- R/manipulate_bg_plays.R
+++ R/manipulate_bg_plays.R
@@ -88,6 +88,43 @@ play_node_to_df <- function(xml_node, player_data = "players", game_data = "item
 }
 
 
+#' Add Winner Data
+#'
+#' Takes a play df and adds a column of winner data.
+#'
+#' Takes a dataframe of gameplay stats as input. Finds all the winners
+#' for that game's id, and makes a list of them. Appends that list back
+#' to the original data frame.
+#'
+#' @param play_df a data frame of gameplay statistics
+#' @return the same play_df with an appended winners column
+add_winner_data <- function(play_df) {
+
+    wins_df <- play_df %>% filter(win == 1)
+
+    games_with_wins <- wins_df %>%
+        arrange(id) %>%
+        pull(id) %>%
+        unique()
+
+    winners_df <- data.frame(
+        id = games_with_wins,
+        winners = rep(NA, length(games_with_wins))
+    )
+
+    winners <- wins_df %>%
+        group_split(id) %>%
+        lapply(pull, name)
+
+    winners_df$winners <- winners
+
+    play_df <- left_join(play_df, winners_df, by = "id")
+
+    play_df
+
+}
+
+
 #' Turn a Board Game Geek file into an R Data Frame
 #'
 #' Takes a Board Game Geek xml list parameter. It outputs a data frame where all
@@ -99,6 +136,8 @@ play_node_to_df <- function(xml_node, player_data = "players", game_data = "item
 play_xml_to_df <- function(xml_play, player_data = "players", game_data = "item") {
 
     lapply(xml_play, play_node_to_df) %>%
-        rbind_list()
+        rbind_list() %>%
+        add_winner_data()
 
 }
+
